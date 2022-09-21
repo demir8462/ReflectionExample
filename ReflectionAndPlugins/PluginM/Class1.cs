36fae364df5b7fe6b82aed9eb8c7759697730a5b
Ali Demir <40163513+demir8462@users.noreply.github.com>
2022-09-21 22:58:46 +0300

    Add files via upload

---
+++ ReflectionAndPlugins/PluginM/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+namespace PluginM
+{
+    interface PluginM
+    {
+        private FormProps prop;
+        string Name { get; set; }
+        string Description { get; set; }
+        FormProps Prop { get; set; }
+
+        public void Run();
+        public Color getFormBackColor();
+    }
+    public class FormProps
+    {
+        Form f;
+        public void setColor(Color c)
+        {
+
+        }
+        public Color getColor()
+        {
+            return 
+        }
+    }
+}
