1b5fce3f04fce5c21d899f55410ca5ccf5502e02
Ali Demir <40163513+demir8462@users.noreply.github.com>
2022-09-22 00:15:59 +0300

    Add files via upload

---
+++ IPlugin/IPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+namespace IPlugin
+{
+    public interface IPLUGIN
+    {
+        
+        public ClassProp PROP { get; set; }
+        string Name { get; set; }
+        string Desc { get; set; }
+        void Run();
+    }
+    public class ClassProp
+    {
+        public Form winform;
+        public delegate void writeConsole(string text);
+        public writeConsole func;
+        Color getBackColor()
+        {
+            return winform.BackColor;
+        }
+        void setBackColor(Color color)
+        {
+            winform.BackColor = color; 
+        }
+        string getTitle()
+        {
+            return winform.Text;
+        }
+        void setTitle(string text)
+        {
+            winform.Text = text;
+        }
+    }
+}
